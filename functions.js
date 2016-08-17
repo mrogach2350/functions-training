753167ecad6b7b1200f4917814ee820d3bfbaee0
mrogach2350 <m.rogach@gmail.com>
2016-08-17 14:56:48 -0700

    Completing 1, 2, and 3

--- functions.js
+++ functions.js
@@ -1,1 +1,29 @@
-// TODO: add your solutions here!
+function combineWords(word1, word2) {
+    return word1 + word2;
+}
+
+var result = combineWords('dog', 'house');
+// console.log(result);
+
+function repeatPhrase(phrase, n) {
+  var idx = 1;
+  while (idx <= n) {
+    console.log(phrase);
+    idx ++;
+  }
+}
+
+// repeatPhrase("Hello", 5);
+
+function toTheNthPower(number, power) {
+  var idx = 1;
+  var num = number;
+  while (idx < power) {
+    num *= number;
+    idx ++;
+  }
+  return num;
+}
+
+var result = toTheNthPower(4, 5);
+console.log(result);
